abeba9933f55a991c7c88b2a55b03bf604478868
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-26 09:31:40 -0500

    added route

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def index():
     a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
     return render_template('dashboard.html', user = a_user)
 
-
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
