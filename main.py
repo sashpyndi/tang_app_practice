868d716d91e0f61bd64b1bed0645bf4ebdfd8146
Sashank Pyndi <70219849+sashpyndi@users.noreply.github.com>
2022-11-29 12:36:34 -0500

    created the database

--- main.py
+++ main.py
@@ -17,37 +17,39 @@ db.init_app(app)
 # Setup models
 with app.app_context():
     db.create_all()   # run under the app context
-tasks = {1:{'title': 'Create Task', 'description': 'User has to create task', 'status': 'backlog'}, 2: {'title': 'Edit Task', 'description': 
-    'User has to edit task', 'status': 'doing'},3:{'title': 'Delete Task', 'description': 'User has to create task' ,'status':'doing'}}
+#tasks = {1:{'title': 'Create Task', 'description': 'User has to create task', 'status': 'backlog'}, 2: {'title': 'Edit Task', 'description': 
+    #'User has to edit task', 'status': 'doing'},3:{'title': 'Delete Task', 'description': 'User has to create task' ,'status':'doing'}}
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='spyndi@uncc.edu').one()
     return render_template('index.html', user=a_user)
 @app.route('/dashboard')
 def viewTasks():
-    a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
+    a_user = db.session.query(User).filter_by(email='spyndi@uncc.edu').one()
+    tasks = db.session.query(Task).all()
     return render_template('dashboard.html', user=a_user, tasks=tasks)
 @app.route('/dashboard/<task_id>')
 def viewTask(task_id):
-    a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
-    return render_template('task.html', user=a_user, task=tasks[int(task_id)])
+  a_user =  db.session.query(User).filter_by(email='spyndi@uncc.edu').one()
+  task = db.session.query(Task).filter_by(id=task_id).one()
+  return render_template('task.html', user=a_user, task=task)
 @app.route('/dashboard/new', methods=['GET', 'POST'])
 def createTask():
- a_user = {'name': 'Sashank', 'email': 'spyndi@uncc.edu'}
  print('request method is', request.method)
- print(tasks)
  if request.method == 'POST':
     my_title = request.form['title']
     my_description = request.form['description']
     my_status = request.form['status']
-    id = len(tasks)+1
-    tasks[id] = {'title': my_title, 'description': my_description, 'status': my_status}
+    new_record = Task(my_title, my_description, my_status)
+    db.session.add(new_record)
+    db.session.commit()
     return redirect(url_for('viewTasks'))
  else:
+    a_user = db.session.query(User).filter_by(email='spyndi@uncc.edu').one()
     return render_template('new.html', user=a_user)
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
